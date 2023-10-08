3d60af1f2af6a9051bc91ebb254dcb6447100160
Ali Yadollahi <aliYadollahi683@gmail.com>
2023-10-08 16:44:36 +0330

    t

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,22 @@
-const sec 
+let user = "x";
+
+const section = document.querySelector("section");
+
+const data = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
+section.addEventListener("click", (e) => {
+  e.preventDefault;
+e.target
+});
+
+// user kodome
+// if user(x,o)=> x
+// update cell ,update data
+// check if win happend
+// if win highlight the cells
+// stop game
+// reset game
