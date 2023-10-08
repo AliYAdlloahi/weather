80927d85ea0039addb60415504e7035e0c9ee797
Ali Yadollahi <aliYadollahi683@gmail.com>
2023-10-08 06:30:14 +0330

    add tailwind

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,38 @@
-const key = "f042b47d6eda46f7bc294734230510";
-const url = new URL("http://api.weatherapi.com/v1/current.json");
-const cityName = document.querySelector("h5");
-const input = document.querySelector("input");
-url.searchParams.append("key", key);
-url.searchParams.append("q", "tehran");
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    cityName.textContent = data.location.name;
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("#buttons button");
+let currentNumber = "";
+let nextNumber = "";
+let operator = null;
+
+buttons.forEach((button) => {
+  console.log(currentNumber, nextNumber, operator);
+  button.addEventListener("click", () => {
+    if (button.innerText.match(/\d/)) {
+      if (currentNumber) {
+        nextNumber += button.innerText;
+        display.value = nextNumber;
+      } else if (!nextNumber) {
+        currentNumber += button.innerText;
+        display.value = currentNumber;
+      }
+    } else if (button.innerText === "C") {
+      currentNumber = "";
+      nextNumber = "";
+      operator = null;
+      display.value = "";
+    } else if (["+", "-", "*", "/"].includes(button.innerText)) {
+      operator = button.innerText;
+      display.value = "";
+    } else if (button.innerText === "=") {
+      if (currentNumber === "" || operator === null) {
+        alert("Please enter a valid expression.");
+      } else {
+        let result = eval(`${currentNumber}${operator}${nextNumber}`);
+        display.value = result;
+        operator = null;
+        currentNumber = result;
+        nextNumber = "";
+      }
+    }
   });
+});
